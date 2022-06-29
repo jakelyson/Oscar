989fb56894a9e2def1329e1cc60cf391d4ff7e95
Joel Ramat <jake_lyson@yahoo.com>
2022-06-29 12:02:30 +0800

    additional projects

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -45,6 +45,26 @@
 			this.btn_browse_link = new System.Windows.Forms.Button();
 			this.txt_link = new System.Windows.Forms.TextBox();
 			this.btn_link = new System.Windows.Forms.Button();
+			this.label5 = new System.Windows.Forms.Label();
+			this.btn_browse_outclient = new System.Windows.Forms.Button();
+			this.txtOutclient = new System.Windows.Forms.TextBox();
+			this.btn_outclient = new System.Windows.Forms.Button();
+			this.label6 = new System.Windows.Forms.Label();
+			this.btn_browse_audit_aged = new System.Windows.Forms.Button();
+			this.txt_audit_aged = new System.Windows.Forms.TextBox();
+			this.btn_audit_aged = new System.Windows.Forms.Button();
+			this.label7 = new System.Windows.Forms.Label();
+			this.btn_browse_deceased = new System.Windows.Forms.Button();
+			this.txt_deceased = new System.Windows.Forms.TextBox();
+			this.btn_deceased = new System.Windows.Forms.Button();
+			this.label8 = new System.Windows.Forms.Label();
+			this.btn_browse_Ptask73 = new System.Windows.Forms.Button();
+			this.txt_Ptask73 = new System.Windows.Forms.TextBox();
+			this.btn_Ptask73 = new System.Windows.Forms.Button();
+			this.label9 = new System.Windows.Forms.Label();
+			this.btn_browse_paper = new System.Windows.Forms.Button();
+			this.txt_Paper = new System.Windows.Forms.TextBox();
+			this.btn_paper = new System.Windows.Forms.Button();
 			this.SuspendLayout();
 			// 
 			// btn_Production
@@ -157,7 +177,7 @@
 			this.btn_Banckruptcy.TabIndex = 8;
 			this.btn_Banckruptcy.Text = "Get Production";
 			this.btn_Banckruptcy.UseVisualStyleBackColor = true;
-			this.btn_Banckruptcy.Click += new System.EventHandler(this.btn_Banckruptcy_Click);
+			this.btn_Banckruptcy.Click += new System.EventHandler(this.btn_Fraud_Click);
 			// 
 			// label4
 			// 
@@ -193,13 +213,213 @@
 			this.btn_link.TabIndex = 12;
 			this.btn_link.Text = "Get Production";
 			this.btn_link.UseVisualStyleBackColor = true;
-			this.btn_link.Click += new System.EventHandler(this.btn_link_Click);
+			this.btn_link.Click += new System.EventHandler(this.btn_Fraud_Click);
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(16, 209);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(49, 13);
+			this.label5.TabIndex = 19;
+			this.label5.Text = "Outclient";
+			// 
+			// btn_browse_outclient
+			// 
+			this.btn_browse_outclient.Location = new System.Drawing.Point(505, 228);
+			this.btn_browse_outclient.Name = "btn_browse_outclient";
+			this.btn_browse_outclient.Size = new System.Drawing.Size(31, 19);
+			this.btn_browse_outclient.TabIndex = 18;
+			this.btn_browse_outclient.Text = "...";
+			this.btn_browse_outclient.UseVisualStyleBackColor = true;
+			this.btn_browse_outclient.Click += new System.EventHandler(this.btnBrowse_Click);
+			// 
+			// txtOutclient
+			// 
+			this.txtOutclient.Location = new System.Drawing.Point(12, 228);
+			this.txtOutclient.Name = "txtOutclient";
+			this.txtOutclient.Size = new System.Drawing.Size(479, 20);
+			this.txtOutclient.TabIndex = 17;
+			// 
+			// btn_outclient
+			// 
+			this.btn_outclient.Location = new System.Drawing.Point(551, 224);
+			this.btn_outclient.Name = "btn_outclient";
+			this.btn_outclient.Size = new System.Drawing.Size(96, 26);
+			this.btn_outclient.TabIndex = 16;
+			this.btn_outclient.Text = "Get Production";
+			this.btn_outclient.UseVisualStyleBackColor = true;
+			this.btn_outclient.Click += new System.EventHandler(this.btn_Production_Click);
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Location = new System.Drawing.Point(16, 257);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(59, 13);
+			this.label6.TabIndex = 23;
+			this.label6.Text = "Audit Aged";
+			// 
+			// btn_browse_audit_aged
+			// 
+			this.btn_browse_audit_aged.Location = new System.Drawing.Point(505, 276);
+			this.btn_browse_audit_aged.Name = "btn_browse_audit_aged";
+			this.btn_browse_audit_aged.Size = new System.Drawing.Size(31, 19);
+			this.btn_browse_audit_aged.TabIndex = 22;
+			this.btn_browse_audit_aged.Text = "...";
+			this.btn_browse_audit_aged.UseVisualStyleBackColor = true;
+			this.btn_browse_audit_aged.Click += new System.EventHandler(this.btnBrowse_Click);
+			// 
+			// txt_audit_aged
+			// 
+			this.txt_audit_aged.Location = new System.Drawing.Point(12, 276);
+			this.txt_audit_aged.Name = "txt_audit_aged";
+			this.txt_audit_aged.Size = new System.Drawing.Size(479, 20);
+			this.txt_audit_aged.TabIndex = 21;
+			// 
+			// btn_audit_aged
+			// 
+			this.btn_audit_aged.Location = new System.Drawing.Point(551, 272);
+			this.btn_audit_aged.Name = "btn_audit_aged";
+			this.btn_audit_aged.Size = new System.Drawing.Size(96, 26);
+			this.btn_audit_aged.TabIndex = 20;
+			this.btn_audit_aged.Text = "Get Production";
+			this.btn_audit_aged.UseVisualStyleBackColor = true;
+			this.btn_audit_aged.Click += new System.EventHandler(this.btn_Fraud_Click);
+			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.Location = new System.Drawing.Point(16, 304);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(86, 13);
+			this.label7.TabIndex = 27;
+			this.label7.Text = "Deceased Ptask";
+			// 
+			// btn_browse_deceased
+			// 
+			this.btn_browse_deceased.Location = new System.Drawing.Point(505, 323);
+			this.btn_browse_deceased.Name = "btn_browse_deceased";
+			this.btn_browse_deceased.Size = new System.Drawing.Size(31, 19);
+			this.btn_browse_deceased.TabIndex = 26;
+			this.btn_browse_deceased.Text = "...";
+			this.btn_browse_deceased.UseVisualStyleBackColor = true;
+			this.btn_browse_deceased.Click += new System.EventHandler(this.btnBrowse_Click);
+			// 
+			// txt_deceased
+			// 
+			this.txt_deceased.Location = new System.Drawing.Point(12, 323);
+			this.txt_deceased.Name = "txt_deceased";
+			this.txt_deceased.Size = new System.Drawing.Size(479, 20);
+			this.txt_deceased.TabIndex = 25;
+			// 
+			// btn_deceased
+			// 
+			this.btn_deceased.Location = new System.Drawing.Point(551, 319);
+			this.btn_deceased.Name = "btn_deceased";
+			this.btn_deceased.Size = new System.Drawing.Size(96, 26);
+			this.btn_deceased.TabIndex = 24;
+			this.btn_deceased.Text = "Get Production";
+			this.btn_deceased.UseVisualStyleBackColor = true;
+			this.btn_deceased.Click += new System.EventHandler(this.btn_Fraud_Click);
+			// 
+			// label8
+			// 
+			this.label8.AutoSize = true;
+			this.label8.Location = new System.Drawing.Point(16, 351);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(72, 13);
+			this.label8.TabIndex = 31;
+			this.label8.Text = "Link Ptask 73";
+			// 
+			// btn_browse_Ptask73
+			// 
+			this.btn_browse_Ptask73.Location = new System.Drawing.Point(505, 370);
+			this.btn_browse_Ptask73.Name = "btn_browse_Ptask73";
+			this.btn_browse_Ptask73.Size = new System.Drawing.Size(31, 19);
+			this.btn_browse_Ptask73.TabIndex = 30;
+			this.btn_browse_Ptask73.Text = "...";
+			this.btn_browse_Ptask73.UseVisualStyleBackColor = true;
+			this.btn_browse_Ptask73.Click += new System.EventHandler(this.btnBrowse_Click);
+			// 
+			// txt_Ptask73
+			// 
+			this.txt_Ptask73.Location = new System.Drawing.Point(12, 370);
+			this.txt_Ptask73.Name = "txt_Ptask73";
+			this.txt_Ptask73.Size = new System.Drawing.Size(479, 20);
+			this.txt_Ptask73.TabIndex = 29;
+			// 
+			// btn_Ptask73
+			// 
+			this.btn_Ptask73.Location = new System.Drawing.Point(551, 366);
+			this.btn_Ptask73.Name = "btn_Ptask73";
+			this.btn_Ptask73.Size = new System.Drawing.Size(96, 26);
+			this.btn_Ptask73.TabIndex = 28;
+			this.btn_Ptask73.Text = "Get Production";
+			this.btn_Ptask73.UseVisualStyleBackColor = true;
+			this.btn_Ptask73.Click += new System.EventHandler(this.btn_Fraud_Click);
+			// 
+			// label9
+			// 
+			this.label9.AutoSize = true;
+			this.label9.Location = new System.Drawing.Point(16, 399);
+			this.label9.Name = "label9";
+			this.label9.Size = new System.Drawing.Size(92, 13);
+			this.label9.TabIndex = 35;
+			this.label9.Text = "Paper Bankruptcy";
+			// 
+			// btn_browse_paper
+			// 
+			this.btn_browse_paper.Location = new System.Drawing.Point(505, 418);
+			this.btn_browse_paper.Name = "btn_browse_paper";
+			this.btn_browse_paper.Size = new System.Drawing.Size(31, 19);
+			this.btn_browse_paper.TabIndex = 34;
+			this.btn_browse_paper.Text = "...";
+			this.btn_browse_paper.UseVisualStyleBackColor = true;
+			this.btn_browse_paper.Click += new System.EventHandler(this.btnBrowse_Click);
+			// 
+			// txt_Paper
+			// 
+			this.txt_Paper.Location = new System.Drawing.Point(12, 418);
+			this.txt_Paper.Name = "txt_Paper";
+			this.txt_Paper.Size = new System.Drawing.Size(479, 20);
+			this.txt_Paper.TabIndex = 33;
+			// 
+			// btn_paper
+			// 
+			this.btn_paper.Location = new System.Drawing.Point(551, 414);
+			this.btn_paper.Name = "btn_paper";
+			this.btn_paper.Size = new System.Drawing.Size(96, 26);
+			this.btn_paper.TabIndex = 32;
+			this.btn_paper.Text = "Get Production";
+			this.btn_paper.UseVisualStyleBackColor = true;
+			this.btn_paper.Click += new System.EventHandler(this.btn_Fraud_Click);
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.label9);
+			this.Controls.Add(this.btn_browse_paper);
+			this.Controls.Add(this.txt_Paper);
+			this.Controls.Add(this.btn_paper);
+			this.Controls.Add(this.label8);
+			this.Controls.Add(this.btn_browse_Ptask73);
+			this.Controls.Add(this.txt_Ptask73);
+			this.Controls.Add(this.btn_Ptask73);
+			this.Controls.Add(this.label7);
+			this.Controls.Add(this.btn_browse_deceased);
+			this.Controls.Add(this.txt_deceased);
+			this.Controls.Add(this.btn_deceased);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.btn_browse_audit_aged);
+			this.Controls.Add(this.txt_audit_aged);
+			this.Controls.Add(this.btn_audit_aged);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.btn_browse_outclient);
+			this.Controls.Add(this.txtOutclient);
+			this.Controls.Add(this.btn_outclient);
 			this.Controls.Add(this.label4);
 			this.Controls.Add(this.btn_browse_link);
 			this.Controls.Add(this.txt_link);
@@ -242,6 +462,26 @@
 		private System.Windows.Forms.Button btn_browse_link;
 		private System.Windows.Forms.TextBox txt_link;
 		private System.Windows.Forms.Button btn_link;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Button btn_browse_outclient;
+		private System.Windows.Forms.TextBox txtOutclient;
+		private System.Windows.Forms.Button btn_outclient;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Button btn_browse_audit_aged;
+		private System.Windows.Forms.TextBox txt_audit_aged;
+		private System.Windows.Forms.Button btn_audit_aged;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.Button btn_browse_deceased;
+		private System.Windows.Forms.TextBox txt_deceased;
+		private System.Windows.Forms.Button btn_deceased;
+		private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.Button btn_browse_Ptask73;
+		private System.Windows.Forms.TextBox txt_Ptask73;
+		private System.Windows.Forms.Button btn_Ptask73;
+		private System.Windows.Forms.Label label9;
+		private System.Windows.Forms.Button btn_browse_paper;
+		private System.Windows.Forms.TextBox txt_Paper;
+		private System.Windows.Forms.Button btn_paper;
 	}
 }
 
--- Form1.cs
+++ Form1.cs
@@ -21,9 +21,11 @@ namespace EoscarProduction
 		public string holidayPath = @"D:\Joel Files\Payroll\holiday.csv";
 		public string clientErrorPath = @"D:\Joel Files\Payroll\ClientError.csv";
 		public string otherIncomePath = @"D:\Joel Files\Payroll\OtherIncome.csv";
-		public string fraudPath = @"D:\Joel Files\Payroll\Fraud.csv";
+		public string fraudPath = @"D:\Joel Files\Payroll\";
 		public string banckruptcyPath = @"D:\Joel Files\Payroll\banckruptcy.csv";
 		public string linkPath = @"D:\Joel Files\Payroll\link.csv";
+		public string outclientpath = @"D:\Joel Files\Payroll\";
+		public string outputPath = @"D:\Joel Files\Payroll\";
 
 		public Form1()
 		{
@@ -53,18 +55,18 @@ namespace EoscarProduction
 
 				xls.Range r = ws.Range["A" + row, "R" + row];
 
-				if (r.Cells[1, 5].Text == "Client Reported")
+				if (r.Cells[1, 6].Text == "Client Reported")
 				{
 					isErrorReport = true;
 				}
 
 				//do client errors 
-				if (isErrorReport && decimal.TryParse(r.Cells[1, 13].Text, out Amount))
+				if (isErrorReport && decimal.TryParse(r.Cells[1, 14].Text, out Amount))
 				{
 					if (Amount != 0)
 					{
-						//3 userid, 
-						fsClientError.WriteLine(string.Format("{0},{1}", r.Cells[1, 3].Text, Amount));
+						//4 userid, 
+						fsClientError.WriteLine(string.Format("{0},{1}", r.Cells[1, 4].Text, Amount));
 					}
 				}
 
@@ -89,7 +91,7 @@ namespace EoscarProduction
 					}
 				}
 
-				if (isErrorReport && ws.Range["M" + row].Value == null)
+				if (isErrorReport && ws.Range["N" + row].Value == null)
 				{
 					//close range
 					Marshal.FinalReleaseComObject(r);
@@ -189,23 +191,219 @@ namespace EoscarProduction
 			MessageBox.Show("Done");
 		}
 
+
+		private void LoadExcelOutclient(String path)
+		{
+			xls.Application xl = new xls.Application();
+			xls.Workbooks wbs = xl.Workbooks;
+			xls.Workbook wb = wbs.Open(path);
+
+			xls.Worksheet ws = wb.Worksheets["Production"];
+
+			long row = 1;
+			bool isErrorReport = false;
+			decimal Amount = 0;
+			StreamWriter fsProd = new StreamWriter(string.Format("{0}outclient.csv",outclientpath), false);
+			StreamWriter fsHoliday = new StreamWriter(string.Format("{0}outclientholiday.csv", outclientpath), false);
+			StreamWriter fsClientError = new StreamWriter(string.Format("{0}outclienterror.csv", outclientpath), false);
+			StreamWriter fsOthereIncome = new StreamWriter(string.Format("{0}outclientotherincome.csv", outclientpath), false);
+
+			while (true)
+			{
+				this.Text = string.Format("Creating Production Files: {0}", row);
+
+				xls.Range r = ws.Range["A" + row, "R" + row];
+
+				if (r.Cells[1, 6].Text == "Client Reported")
+				{
+					isErrorReport = true;
+				}
+
+				//do client errors 
+				if (isErrorReport && decimal.TryParse(r.Cells[1, 14].Text, out Amount))
+				{
+					if (Amount != 0)
+					{
+						//3 userid, 
+						fsClientError.WriteLine(string.Format("{0},{1}", r.Cells[1, 4].Text, Amount));
+					}
+				}
+
+				if (!isErrorReport)
+				{
+					if (r.Cells[1, 14].Text != string.Empty) //N
+					{
+						//do total amount here
+						fsProd.WriteLine(string.Format("{0},{1}", r.Cells[1, 14].Text, r.Cells[1, 13].Text));
+					}
+					else if (r.Cells[1, 11].Text != string.Empty) //holiday - K 11
+					{
+						if (decimal.TryParse(r.Cells[1, 11].Text, out Amount))
+						{
+							if (Amount != 0)
+							{
+								//do holiday here
+								//2 -- date, 11-holiday,1- userid
+								fsHoliday.WriteLine(string.Format("{0},{1},{2:0000}", r.Cells[1, 2].Text, Amount, r.Cells[1, 1].Text));
+							}
+						}
+					}
+				}
+
+				if (isErrorReport && ws.Range["N" + row].Value == null)
+				{
+					//close range
+					Marshal.FinalReleaseComObject(r);
+					break;
+				}
+
+				//close range
+				Marshal.FinalReleaseComObject(r);
+				row += 1;
+			}
+
+
+			//release all com objects 
+			Marshal.FinalReleaseComObject(ws);
+
+			//otherincome
+			try
+			{
+				ws = wb.Worksheets["Other Income"];
+				row = 3;
+				int blank = 0;
+				int userid = 0;
+				string remarks = string.Empty;
+				decimal amount = 0;
+				int prevUserid = 0;
+				while (true)
+				{
+					this.Text = string.Format("Creating Other Income Files: {0}", row);
+					xls.Range r = ws.Range["A" + row, "I" + row];
+
+					if (r.Cells[1, 1].Text + r.Cells[1, 2].Text != string.Empty)
+					{
+						blank = 0;
+						if (int.TryParse(r.Cells[1, 1].Text, out userid))
+						{
+							if (prevUserid != userid)
+							{
+								if (prevUserid != 0)
+								{
+									fsOthereIncome.WriteLine(string.Format("{0:0000}, {1}, {2}", prevUserid, remarks, amount));
+								}
+								prevUserid = userid;
+								amount = decimal.Parse(r.Cells[8].Text);
+							}
+							else
+							{
+								amount += decimal.Parse(r.Cells[8].Text);
+							}
+						}
+						else
+						{
+
+							if (prevUserid != 0)
+							{
+								fsOthereIncome.WriteLine(string.Format("{0:0000}, {1}, {2}", prevUserid, remarks, amount));
+							}
+							prevUserid = 0;
+							amount = 0;
+							remarks = r.Cells[1, 1].Text + r.Cells[1, 2].Text;
+						}
+					}
+					else
+					{
+						blank += 1;
+						if (blank > 5)
+						{
+							//write last person
+							fsOthereIncome.WriteLine(string.Format("{0:0000}, {1}, {2}", prevUserid, remarks, amount));
+
+							//close range
+							Marshal.FinalReleaseComObject(r);
+							break;
+						}
+					}
+					Marshal.FinalReleaseComObject(r);
+					row += 1;
+				}
+			}
+			catch (Exception)
+			{
+			}
+
+			//release all com objects 
+			Marshal.FinalReleaseComObject(ws);
+
+			wb.Close();
+			Marshal.FinalReleaseComObject(wb);
+
+			xl.Quit();
+			Marshal.FinalReleaseComObject(xl);
+
+			fsProd.Close();
+			fsHoliday.Close();
+			fsClientError.Close();
+			fsOthereIncome.Close();
+
+			MessageBox.Show("Done");
+		}
+
 		private void btn_Production_Click(object sender, EventArgs e)
 		{
 			//LoadExcel(@"C:\Joel Files\Payroll\20200511\20200426-20200510 eOscar PS.xlsx");
-			LoadExcel(txtExcelFile.Text);
+			switch (((Button)sender).Name) {
+				case "btn_outclient":
+					LoadExcelOutclient(txtOutclient.Text);
+					break;
+				default:
+					LoadExcel(txtExcelFile.Text);
+					break; 
+			}
+			
 		}
 
 		private void btnBrowse_Click(object sender, EventArgs e)
 		{
-			openFileDialog1.Filter = "Eoscar PS|*Eoscar*.xlsx";
 			openFileDialog1.CheckFileExists = true;
-			if (openFileDialog1.ShowDialog() == DialogResult.OK)
+			switch (((Button)sender).Name)
 			{
-				txtExcelFile.Text = openFileDialog1.FileName;
+				case "btn_browse_paper":
+					openFileDialog1.Filter = "Paper Bankruptcy|*Paper*.xlsx";
+					if (openFileDialog1.ShowDialog() == DialogResult.OK) txt_Paper.Text = openFileDialog1.FileName;
+					break;
+				case "btn_browse_Ptask73":
+					openFileDialog1.Filter = "PTask73|*Ptask73*.xlsx";
+					if (openFileDialog1.ShowDialog() == DialogResult.OK) txt_Ptask73.Text = openFileDialog1.FileName;
+					break;
+				case "btn_browse_deceased":
+					openFileDialog1.Filter = "Deceased|*Deceased*.xlsx";
+					if (openFileDialog1.ShowDialog() == DialogResult.OK) txt_deceased.Text = openFileDialog1.FileName;
+					break;
+				case "btn_browse_outclient":
+					openFileDialog1.Filter = "Outclient|*outclient*.xlsx";
+					if (openFileDialog1.ShowDialog() == DialogResult.OK) txtOutclient.Text = openFileDialog1.FileName;
+					break;
+				case "btn_browse_audit_aged":
+					openFileDialog1.Filter = "Audit Aged|*Audit Aged*.xlsx";
+					if (openFileDialog1.ShowDialog() == DialogResult.OK)
+					{
+						txt_audit_aged.Text = openFileDialog1.FileName;
+					}
+					break;
+				default:
+					openFileDialog1.Filter = "Eoscar PS|*Eoscar*.xlsx";
+					if (openFileDialog1.ShowDialog() == DialogResult.OK)
+					{
+						txtExcelFile.Text = openFileDialog1.FileName;
+					}
+					break;
 			}
+
 		}
 
-		private void btn_browse_fraud_Click(object sender, EventArgs e)
+		private void btn_browse_fraud_Click(object sender, EventArgs e) 
 		{
 			openFileDialog1.Filter = "Fraud PS|*Fraud*.xlsx";
 			openFileDialog1.CheckFileExists = true;
@@ -215,58 +413,97 @@ namespace EoscarProduction
 			}
 		}
 
-		private void btn_Fraud_Click(object sender, EventArgs e)
-		{
-			if (txtFraudProduction.Text.Trim() == string.Empty ) { return;  }
+		private void getPayroll(string excelPath , string output, string errorPath = null ) {
 
-			StreamWriter fraud = File.CreateText(fraudPath);
+			if (excelPath == string.Empty) { return; }
+
+			StreamWriter fraud = File.CreateText(output);
+			bool isOutputError = false;
+			StreamWriter outputError = null; 
+			if (!(errorPath is  null) )
+			{
+				outputError = File.CreateText(errorPath);
+				isOutputError = true; 
+			}
 
 			xls.Application xl = new xls.Application();
 			xls.Workbooks wbs = xl.Workbooks;
-			xls.Workbook wb = wbs.Open(txtFraudProduction.Text);
+			xls.Workbook wb = wbs.Open(excelPath);
 			xls.Worksheet ws = wb.Worksheets["Production"];
 
 			int blankCounter = 0;
 			int iRow = 2;
-
+			int errorRow = 1;
+			bool onErrorRange = false;
 			string userid = "";
-			decimal amount = 0; 
+			decimal amount = 0;
 
 			while (true)
 			{
-				xls.Range r = ws.Range["A" + iRow , "H"+ iRow];
+				xls.Range r = ws.Range["A" + iRow, "H" + iRow];
 
 
 				if (r.Cells[1, 1].Text == string.Empty)
 				{
 					blankCounter += 1;
+
+					if (userid != string.Empty ) fraud.WriteLine(string.Format("{0},{1}", userid, amount));
+					userid = string.Empty;
 				}
 				else
 				{
 					blankCounter = 1;
-					if (userid == r.Cells[1, 2].Text && userid != string.Empty )
+
+
+					//do client errors 
+					if (isOutputError && onErrorRange && decimal.TryParse(r.Cells[1, errorRow].Text, out amount))
 					{
-						amount += decimal.Parse(r.Cells[1, 8].Text);
+						if (amount != 0)
+						{
+							//3 userid, 
+							outputError.WriteLine(string.Format("{0},{1}", r.Cells[1, 1].Text, amount));
+						}
 					}
-					else
+
+					if (!onErrorRange)
 					{
+						//check if on error range 
+						if (ws.Range["F"+iRow].Text == "Error Amount")
+						{
+							onErrorRange = true;
+							errorRow = 6; 
+						}
+						else if (ws.Range["G" + iRow].Text == "Error Amount")
+						{
+							onErrorRange = true;
+							errorRow = 11; 
+						}
+						else { 
+						
+							if (userid == r.Cells[1, 2].Text && userid != string.Empty)
+							{
+								amount += decimal.Parse(r.Cells[1, 8].Text);
+							}
+							else
+							{
+								//insert on text file
+								if (userid != string.Empty) fraud.WriteLine(string.Format("{0},{1}", userid, amount));
 
-						//insert on text file
-						fraud.WriteLine ( string.Format("{0},{1}", userid , amount));
-
-						userid = r.Cells[1, 2].Text; 
-						amount = decimal.Parse(r.Cells[1,8 ].Text);
+								userid = r.Cells[1, 2].Text;
+								amount = decimal.Parse(r.Cells[1, 8].Text);
+							}
+						}
 					}
 				}
 
-				if (blankCounter > 5)
+				if (blankCounter > 10)
 				{
 					break;
 				}
-				iRow +=1;
+				iRow += 1;
 			}
 			//write last person
-			fraud.WriteLine(string.Format("{0},{1}", userid, amount));
+			if(userid != "") fraud.WriteLine(string.Format("{0},{1}", userid, amount));
 
 			//release all com objects 
 			Marshal.FinalReleaseComObject(ws);
@@ -278,8 +515,37 @@ namespace EoscarProduction
 			Marshal.FinalReleaseComObject(xl);
 
 			fraud.Close();
+			outputError.Close();
 
 			MessageBox.Show("Done");
+		}
+
+		private void btn_Fraud_Click(object sender, EventArgs e)
+		{
+			switch (((Button)sender).Name)
+			{
+				case "btn_link":
+					getPayroll(txt_link.Text, outputPath + "Link.csv", outputPath + "LinkError.csv");
+					break;
+				case "btn_Banckruptcy":
+					getPayroll(txtBanckruptcyProduction.Text, outputPath + "Bankruptcy.csv", outputPath + "BankruptcyError.csv");
+					break; 
+				case "btn_paper":
+					getPayroll(txt_Paper.Text, outputPath + "Paper.csv", outputPath + "PaperError.csv");
+					break;
+				case "btn_Ptask73":
+					getPayroll(txt_Ptask73.Text, outputPath + "Ptask73.csv", outputPath + "Ptask73Error.csv");
+					break;
+				case "btn_deceased":
+					getPayroll(txt_deceased.Text, outputPath + "deceased.csv", outputPath + "deceasedError.csv");
+					break;
+				case "btn_audit_aged":
+					getPayroll(txt_audit_aged.Text, outputPath + "AuditAge.csv", outputPath + "AuditAgedError.csv"); 
+					break;
+				default:
+					getPayroll(txtFraudProduction.Text, outputPath + "Fraud.csv", outputPath + "fraud_error.csv");
+					break;
+			}
 		}
 
 		private void btn_browse_Banckruptcy_Click(object sender, EventArgs e)
@@ -435,6 +701,7 @@ namespace EoscarProduction
 
 			MessageBox.Show("Done");
 		}
+
 	}
 	
 }
